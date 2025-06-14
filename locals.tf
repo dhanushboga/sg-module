1fb0c0be01d4bef8877864ddf9b58f2befc0690c
dhanushboga <dhanushboga555@gmail.com>
2025-06-14 22:03:09 +0530

    sg

---
+++ locals.tf
@@ -0,0 +1,3 @@
+locals {
+  sg_name_final = "${var.Project_name}-${var.environment}-${var.sg_name}"
+}
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "Project_name" {
+  
+}
+
+variable "environment" {
+  
+}
+
+variable "vpc_id" {
+  
+}
+
+variable "common_tags" {
+  default = {
+    project = "expense"
+    environment = "dev"
+    terraform = "true"
+  }
+}
+
+variable "sg_tags" {
+  default = {}
+}
+
+variable "sg_name" {
+  
+}
